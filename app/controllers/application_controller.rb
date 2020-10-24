cc801a366dd1f61a77f1ea6dfe6f6307cf7db22f
vagrant <vagrant@localhost.localdomain>
2020-10-24 01:05:25 +0000

    [add]cart_product

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
 
   protect_from_forgery with: :exception
    before_action :configure_permitted_parameters, if: :devise_controller?
+   before_action :set_current_end_user
 
 
     def after_sign_in_path_for(resource)
@@ -12,23 +13,16 @@ class ApplicationController < ActionController::Base
         root_path(resource)
     end
 
+    def set_current_end_user
+      @current_end_user = EndUser.find_by(id: session[:end_user_id])
+    end
+
+
    protected
 
      def configure_permitted_parameters
        devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name, :first_name, :last_name_kana, :first_name_kana, :address, :phone_number, :deleted, :zipcord])
      end
 
-private
-  #セッションの作成
-  def current_end_user
-    # セッションから取得したcart_idを元にCartテーブルからCart情報を取得
-    current_end_user = EndUser.find_by(id: session[:cart_id])
-    # Cart情報が存在しない場合、@current_cartを作成
-    current_end_user = EndUser.create unless current_end_user
-    # 取得したCart情報よりIDを取得し、セッションに設定
-    session[:end_user_id] = current_end_user.id
-    # Cart情報を返却
-    current_end_user
-  end
 end
 
